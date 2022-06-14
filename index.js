d1bc424da06eca68826a019220fa00d61591b545
sid-al <sendplusx@gmail.com>
2022-06-14 14:16:46 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Your code goes here
+
+// setup an event listner for DOMContentLoaded;
+
+document.addEventListener('DOMContentLoaded', function(){
+  console.log('The DOM has loaded')
+  updatedDOM()
+});
+
+function updatedDOM(){
+  const p = document.getElementById('text')
+  p.innerHTML = 'This is really cool!';
+}
+
+// const p = document.querySelector('p')
+// console.log(p);
+
+// p.innerText = 'This is really cool!'
